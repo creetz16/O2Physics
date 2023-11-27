db5025f4cae966906337fbc8f076c2bd1012916e
creetz16 <carolina@reetz.name>
2023-11-27 17:23:48 +0100

    Add more histos

--- PWGLF/Tasks/QC/lfpropStudy.cxx
+++ PWGLF/Tasks/QC/lfpropStudy.cxx
@@ -9,13 +9,16 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 //
+/// \file lfpropStudy.cxx
+/// \since 27-11-2023
+/// \author Carolina Reetz <c.reetz@cern.ch>
 /// \brief QA task to study properties of propagated tracks
 
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
-#include "Framework/ASoAHelpers.h"
 #include "ReconstructionDataFormats/Track.h"
+#include "ReconstructionDataFormats/DCA.h"
 #include "Common/Core/RecoDecay.h"
 #include "Common/Core/trackUtilities.h"
 #include "Common/Core/TrackSelection.h"
@@ -23,8 +26,8 @@
 #include "Common/DataModel/TrackSelectionTables.h"
 #include "Common/DataModel/EventSelection.h"
 #include "Common/DataModel/Centrality.h"
-#include "Common/DataModel/PIDResponse.h"
 #include "Common/DataModel/Multiplicity.h"
+#include "Common/DataModel/PIDResponse.h"
 #include "TPDGCode.h"
 
 using namespace o2;
@@ -40,15 +43,14 @@ struct lfpropStudy {
   ConfigurableAxis axisDCAz{"axisDCAz", {1000, -10.f, 10.f}, "DCA_{z} (cm)"};
   ConfigurableAxis axisMom{"axisMom", {1000, 0.0f, 10.f}, "p (GeV/c)"};
 
-  Configurable<float> d_pTMin{"d_pTMin", 0.3, "minimum track mommentum"};
+  Configurable<bool> d_noITS{"d_noITS", true, "flag to select tracks without ITS"};
+  Configurable<float> d_pTMin{"d_pTMin", 0.3, "minimum track momentum"};
   Configurable<float> d_TPCrowsMin{"d_TPCrowsMin", 70, "minimum number of TPC crossed rows"};
   Configurable<float> d_TPCrowsOverFindMin{"d_TPCrowsOverFindMin", 0.8, "minimum for ratio of TPC crossed rows over findable"};
   
   HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
 
-  // Filter collisionFilter = (aod::evsel::sel8 == true);
-  // Filter etaFilter = (nabs(aod::track::eta) < 0.8f);
-  // Filter ptFilter = (aod::track::pt > d_pTMin);
+  Filter collisionFilter = (aod::evsel::sel8 == true);
 
   void init(InitContext const&)
   {
@@ -58,27 +60,35 @@ struct lfpropStudy {
     histos.add("hPxEl", "hPxEl", kTH1F, {axisMom});
     histos.add("hPyEl", "hPyEl", kTH1F, {axisMom});
     histos.add("hPzEl", "hPzEl", kTH1F, {axisMom});
+    histos.add("hPtEl", "hPtEl", kTH1F, {axisMom});
     histos.add("hPxPi", "hPxPi", kTH1F, {axisMom});
     histos.add("hPyPi", "hPyPi", kTH1F, {axisMom});
     histos.add("hPzPi", "hPzPi", kTH1F, {axisMom});
+    histos.add("hPtPi", "hPtPi", kTH1F, {axisMom});
     histos.add("hPxKa", "hPxKa", kTH1F, {axisMom});
     histos.add("hPyKa", "hPyKa", kTH1F, {axisMom});
     histos.add("hPzKa", "hPzKa", kTH1F, {axisMom});
+    histos.add("hPtKa", "hPtKa", kTH1F, {axisMom});
     histos.add("hPxPr", "hPxPr", kTH1F, {axisMom});
     histos.add("hPyPr", "hPyPr", kTH1F, {axisMom});
     histos.add("hPzPr", "hPzPr", kTH1F, {axisMom});
+    histos.add("hPtPr", "hPtPr", kTH1F, {axisMom});
     histos.add("hPxDe", "hPxDe", kTH1F, {axisMom});
     histos.add("hPyDe", "hPyDe", kTH1F, {axisMom});
     histos.add("hPzDe", "hPzDe", kTH1F, {axisMom});
+    histos.add("hPtDe", "hPtDe", kTH1F, {axisMom});
     histos.add("hPxTr", "hPxTr", kTH1F, {axisMom});
     histos.add("hPyTr", "hPyTr", kTH1F, {axisMom});
     histos.add("hPzTr", "hPzTr", kTH1F, {axisMom});
+    histos.add("hPtTr", "hPtTr", kTH1F, {axisMom});
     histos.add("hPxHe", "hPxHe", kTH1F, {axisMom});
     histos.add("hPyHe", "hPyHe", kTH1F, {axisMom});
     histos.add("hPzHe", "hPzHe", kTH1F, {axisMom});
+    histos.add("hPtHe", "hPtHe", kTH1F, {axisMom});
     histos.add("hPxAl", "hPxAl", kTH1F, {axisMom});
     histos.add("hPyAl", "hPyAl", kTH1F, {axisMom});
     histos.add("hPzAl", "hPzAl", kTH1F, {axisMom});
+    histos.add("hPtAl", "hPtAl", kTH1F, {axisMom});
 
     // DCA
     histos.add("hDCAxyEl", "hDCAxyEl", kTH1F, {axisDCAxy});
@@ -99,61 +109,70 @@ struct lfpropStudy {
     histos.add("hDCAzAl", "hDCAzAl", kTH1F, {axisDCAz});
   }
 
-  void processData(aod::Collision const& collision, Tracks const& Tracks)
+  void processData(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& collision, Tracks const& Tracks)
   {
     histos.fill(HIST("hEventCounter"), 0.5);
     for (auto& track : Tracks) {
 
       // track selection
-      if (track.hasITS()) continue; // only look at tracks which start outside the ITS
+      if (d_noITS && track.hasITS()) continue; // optional: only look at tracks which start outside the ITS
+      if (track.trackType() == aod::track::TrackIU) continue; // only look at tracks which were propagated
       if (track.tpcNClsCrossedRows() < d_TPCrowsMin || track.tpcCrossedRowsOverFindableCls() < d_TPCrowsOverFindMin) continue;
 
       if (track.pidForTracking() == o2::track::PID::Electron) {
         histos.fill(HIST("hPxEl"), track.px());
         histos.fill(HIST("hPyEl"), track.py());
         histos.fill(HIST("hPzEl"), track.pz());
+        histos.fill(HIST("hPtEl"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyEl"), track.dcaXY());
         histos.fill(HIST("hDCAzEl"), track.dcaZ());
       } else if (track.pidForTracking() == o2::track::PID::Pion) {
         histos.fill(HIST("hPxPi"), track.px());
         histos.fill(HIST("hPyPi"), track.py());
         histos.fill(HIST("hPzPi"), track.pz());
+        histos.fill(HIST("hPtPi"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyPi"), track.dcaXY());
         histos.fill(HIST("hDCAzPi"), track.dcaZ());
       } else if (track.pidForTracking() == o2::track::PID::Kaon) {
         histos.fill(HIST("hPxKa"), track.px());
         histos.fill(HIST("hPyKa"), track.py());
         histos.fill(HIST("hPzKa"), track.pz());
+        histos.fill(HIST("hPtKa"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyKa"), track.dcaXY());
         histos.fill(HIST("hDCAzKa"), track.dcaZ());
       } else if (track.pidForTracking() == o2::track::PID::Proton) {
         histos.fill(HIST("hPxPr"), track.px());
         histos.fill(HIST("hPyPr"), track.py());
         histos.fill(HIST("hPzPr"), track.pz());
+        histos.fill(HIST("hPtPr"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyPr"), track.dcaXY());
         histos.fill(HIST("hDCAzPr"), track.dcaZ());
       } else if (track.pidForTracking() == o2::track::PID::Deuteron) {
         histos.fill(HIST("hPxDe"), track.px());
         histos.fill(HIST("hPyDe"), track.py());
         histos.fill(HIST("hPzDe"), track.pz());
+        histos.fill(HIST("hPtDe"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyDe"), track.dcaXY());
         histos.fill(HIST("hDCAzDe"), track.dcaZ());
       } else if (track.pidForTracking() == o2::track::PID::Triton) {
         histos.fill(HIST("hPxTr"), track.px());
         histos.fill(HIST("hPyTr"), track.py());
         histos.fill(HIST("hPzTr"), track.pz());
+        histos.fill(HIST("hPtTr"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyTr"), track.dcaXY());
         histos.fill(HIST("hDCAzTr"), track.dcaZ());
       } else if (track.pidForTracking() == o2::track::PID::Helium3) {
         histos.fill(HIST("hPxHe"), track.px());
         histos.fill(HIST("hPyHe"), track.py());
         histos.fill(HIST("hPzHe"), track.pz());
+        histos.fill(HIST("hPtHe"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyHe"), track.dcaXY());
         histos.fill(HIST("hDCAzHe"), track.dcaZ());
       } else if (track.pidForTracking() == o2::track::PID::Alpha) {
         histos.fill(HIST("hPxAl"), track.px());
         histos.fill(HIST("hPyAl"), track.py());
         histos.fill(HIST("hPzAl"), track.pz());
+        histos.fill(HIST("hPtAl"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyAl"), track.dcaXY());
         histos.fill(HIST("hDCAzAl"), track.dcaZ());
       }
@@ -161,36 +180,69 @@ struct lfpropStudy {
   }
   PROCESS_SWITCH(lfpropStudy, processData, "process data", true);
 
-  void processMC(aod::Collision const& collision, TracksLabeled const& Tracks, aod::McParticles const& particlesMC)
+  void processMC(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& collision, TracksLabeled const& Tracks, aod::McParticles const& particlesMC)
   {
     histos.fill(HIST("hEventCounter"), 0.5);
     for (auto& track : Tracks) {
 
-      if (!track.has_mcParticle() || track.mcParticleId() <= -1 || track.mcParticleId() > particlesMC.size()) continue; // only look at tracks which had right PID assgined
-      if (track.hasITS()) continue; // only look at tracks which start outside the ITS
+      if (!track.has_mcParticle() || track.mcParticleId() <= -1 || track.mcParticleId() > particlesMC.size()) continue;
+      if (d_noITS && track.hasITS()) continue; // optional: only look at tracks which start outside the ITS
+      if (track.trackType() == aod::track::TrackIU) continue; // only look at tracks which were propagated
 
       if (track.mcParticle().pdgCode() == kElectron) {
+        histos.fill(HIST("hPxEl"), track.px());
+        histos.fill(HIST("hPyEl"), track.py());
+        histos.fill(HIST("hPzEl"), track.pz());
+        histos.fill(HIST("hPtEl"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyEl"), track.dcaXY());
         histos.fill(HIST("hDCAzEl"), track.dcaZ());
       } else if (track.mcParticle().pdgCode() == kPiPlus) {
+        histos.fill(HIST("hPxPi"), track.px());
+        histos.fill(HIST("hPyPi"), track.py());
+        histos.fill(HIST("hPzPi"), track.pz());
+        histos.fill(HIST("hPtPi"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyPi"), track.dcaXY());
         histos.fill(HIST("hDCAzPi"), track.dcaZ());
       } else if (track.mcParticle().pdgCode() == kKPlus) {
+        histos.fill(HIST("hPxKa"), track.px());
+        histos.fill(HIST("hPyKa"), track.py());
+        histos.fill(HIST("hPzKa"), track.pz());
+        histos.fill(HIST("hPtKa"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyKa"), track.dcaXY());
         histos.fill(HIST("hDCAzKa"), track.dcaZ());
       } else if (track.mcParticle().pdgCode() == kProton) {
+        histos.fill(HIST("hPxPr"), track.px());
+        histos.fill(HIST("hPyPr"), track.py());
+        histos.fill(HIST("hPzPr"), track.pz());
+        histos.fill(HIST("hPtPr"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyPr"), track.dcaXY());
         histos.fill(HIST("hDCAzPr"), track.dcaZ());
       } else if (track.mcParticle().pdgCode() == 1000010020) {
+        histos.fill(HIST("hPxDe"), track.px());
+        histos.fill(HIST("hPyDe"), track.py());
+        histos.fill(HIST("hPzDe"), track.pz());
+        histos.fill(HIST("hPtDe"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyDe"), track.dcaXY());
         histos.fill(HIST("hDCAzDe"), track.dcaZ());
       } else if (track.mcParticle().pdgCode() == 1000010030) {
+        histos.fill(HIST("hPxTr"), track.px());
+        histos.fill(HIST("hPyTr"), track.py());
+        histos.fill(HIST("hPzTr"), track.pz());
+        histos.fill(HIST("hPtTr"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyTr"), track.dcaXY());
         histos.fill(HIST("hDCAzTr"), track.dcaZ());
       } else if (track.mcParticle().pdgCode() == 1000020030) {
+        histos.fill(HIST("hPxHe"), track.px());
+        histos.fill(HIST("hPyHe"), track.py());
+        histos.fill(HIST("hPzHe"), track.pz());
+        histos.fill(HIST("hPtHe"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyHe"), track.dcaXY());
         histos.fill(HIST("hDCAzHe"), track.dcaZ());
       } else if (track.mcParticle().pdgCode() == 1000020040) {
+        histos.fill(HIST("hPxAl"), track.px());
+        histos.fill(HIST("hPyAl"), track.py());
+        histos.fill(HIST("hPzAl"), track.pz());
+        histos.fill(HIST("hPtAl"), sqrt(track.px()*track.px() + track.py()*track.py()));
         histos.fill(HIST("hDCAxyAl"), track.dcaXY());
         histos.fill(HIST("hDCAzAl"), track.dcaZ());
       } 
